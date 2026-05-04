c42461a66dcd1827114e08d878303a92357a0e56
Prem Mourya <premmourya084@gmail.com>
2026-05-04 21:49:17 +0530

    ping feature added

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ app.use(
 app.get("/", (req, res) => {
   res.send("server is running on port 3000");
 });
+app.get("/ping", (req, res) => {
+  res.send("ok");
+});
 app.post("/api/login", authController.userLogin);
 app.post("/api/admin/login", authController.adminLogin);
 app.post("/api/register", authController.register);
